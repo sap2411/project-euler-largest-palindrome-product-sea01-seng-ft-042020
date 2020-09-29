31ef3febf7417e5c88212b81e733b331fca7abf2
sap2411 <steven.parsons.us@gmail.com>
2020-09-29 14:09:36 -0700

    Done.

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,29 @@
 # Implement your object-oriented solution here!
+class LargestPalindromeProduct
+    def answer
+        high = 999
+        ans = 0
+        target_high = 0
+        target_low = 0
+        while high > 99
+            low = high
+            while low > 99
+                num = high * low
+                if num < ans
+                    break
+                elsif num === num.to_s.reverse.to_i
+                    ans = num
+                    target_high = high
+                    target_low = low
+                    break
+                end
+                low -= 1
+            end
+            if high * high < target_high * target_low
+                break
+            end
+            high -= 1
+        end
+        ans
+    end
+end
